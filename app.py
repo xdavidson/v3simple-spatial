366d6c18a7acca6431867ad8e1e9b7fd7cc8732c
Lisa Davidson <llu@redhat.com>
2017-06-28 16:23:42 -0400

    training

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from random_words import RandomWords
 
 @route('/')
 def index():
-    return "<h1> STEVE IS AWESOME GIVE ME $$$$</h1>"
+    return "<h1> HELLO LISA</h1>"
 
 
 # since this is a read only talk to the replicas
